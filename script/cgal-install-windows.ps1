0a3f759a038b888725a566a044cc902178d5065c
Gianmarco <gianmarco.picarella98@gmail.com>
2023-09-01 23:11:52 +0200

    fixed yaml-tool build target

--- script/cgal-install-windows.ps1
+++ script/cgal-install-windows.ps1
@@ -3,5 +3,6 @@ $VCPKG_EXE = Join-Path $VCPKG_BASE \vcpkg.exe
 $CGAL_TRIPLET = "x64-windows" # or x86-windows, based on the build target
 
 Invoke-Expression (Join-Path $VCPKG_BASE \bootstrap-vcpkg.bat)
-Invoke-Expression ($VCPKG_EXE + " install yasm-tool:" + $CGAL_TRIPLET)
+# because of a bug with gmp in vcpkg for windows the x86 version is needed
+Invoke-Expression ($VCPKG_EXE + " install yasm-tool:x86-windows") 
 Invoke-Expression ($VCPKG_EXE + " install cgal --triplet " + $CGAL_TRIPLET)
