caaa4644ccc986fc6e0d7f852cd655e924b6e040
Gianmarco <gianmarco.picarella98@gmail.com>
2023-09-06 12:31:12 +0200

    Compilation with multiple files, VS22 shows header files and build dir is always erased before building the project

--- script/src-build-windows.ps1
+++ script/src-build-windows.ps1
@@ -3,4 +3,5 @@ $BUILD_DIR = "../build"
 $CGAL_CC_PATH = "../dep/vcpkg/scripts/buildsystems/vcpkg.cmake"
 $VCPP_VERSION = '"Visual Studio 17 2022"' # change if needed
 
+Remove-Item -Recurse -Force $BUILD_DIR # clean build dir first
 Invoke-Expression ("cmake -S " + $SRC_DIR + " -B " + $BUILD_DIR + " -G " + $VCPP_VERSION + " --toolchain " + $CGAL_CC_PATH)
