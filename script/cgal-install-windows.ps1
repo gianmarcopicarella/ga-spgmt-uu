ccf190314cd4a509ac4ef2d648f3663e3b59ef7d
Gianmarco <gianmarco.picarella98@gmail.com>
2023-10-16 14:23:06 +0200

    Installed HPX library for parallel programming

--- script/cgal-install-windows.ps1
+++ script/cgal-install-windows.ps1
@@ -4,6 +4,9 @@ $VCPKG_EXE = Join-Path $VCPKG_BASE \vcpkg.exe
 $env:VCPKG_DEFAULT_TRIPLET = "x64-windows"
 
 Invoke-Expression (Join-Path $VCPKG_BASE \bootstrap-vcpkg.bat)
+#Update VCPKG
+Invoke-Expression ($VCPKG_EXE + " update") 
+
 # because of a bug with gmp in vcpkg for windows the x86 version is needed
 Invoke-Expression ($VCPKG_EXE + " install yasm-tool:x86-windows") 
 Invoke-Expression ($VCPKG_EXE + " install cgal")
@@ -12,3 +15,8 @@ Invoke-Expression ($VCPKG_EXE + " install cgal")
 
 # TEMPORARY TEST
 # Invoke-Expression ($VCPKG_EXE + " install qt5")
+
+# Install parallel programming library
+# READ the procedure: https://github.com/microsoft/vcpkg/issues/1605
+# ALSO keep paths as short as possible
+Invoke-Expression ($VCPKG_EXE + " install hpx")
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,81 @@
+//
+//#include "../common/Types.h"
+//
+//using namespace SPGMT;
+//
+//int main()
+//{
+//	/*Plane p{ 10.f, 1.f, 2.03f, -4.f };
+//	Plane o{ p.point(), -p.orthogonal_direction() };
+//
+//	std::cout << p << std::endl;
+//	std::cout << o << std::endl;*/
+//
+//	return 0;
+//}
 
-int main()
+#include "../common/Types.h"
+
+#include <CGAL/boost/graph/Dual.h>
+#include <CGAL/boost/graph/helpers.h>
+
+#include <hpx/init.hpp>
+#include <hpx/hpx.hpp>
+
+#include <iostream>
+#include <chrono>
+
+using namespace SPGMT;
+
+//--hpx:threads = N
+
+int hpx_func()
 {
+	using std::chrono::high_resolution_clock;
+	using std::chrono::duration_cast;
+	using std::chrono::duration;
+	using std::chrono::milliseconds;
+
+	std::vector<int> values(1000000000);
+	std::iota(values.begin(), values.end(), 0);
+
+	auto t1 = high_resolution_clock::now();
+
+	hpx::parallel::sort(
+		hpx::execution::par,
+		values.begin(), values.end(),
+		[](int a, int b)
+		{
+			return a > b;
+		}
+	);
+
+	auto t2 = high_resolution_clock::now();
+
+	auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+	std::cout << "Elapsed time: " << ms_int.count() << std::endl;
+
+	return hpx::local::finalize();
+}
+
+int main(int argc, char* argv[]) {
+
+	hpx::local::init(hpx_func, argc, argv);
+
+
+
+	//std::cout << "abc" << std::endl;
+
+	/*typedef CGAL::Dual<Plane> DualPlane;
+
+	DualPlane dp{ Plane{10.f, 1.f, 2.03f, -4.f} };
+
+	std::cout << CGAL::num_vertices(dp) << std::endl;*/
+
 	return 0;
+
+	/*unsigned int n = std::thread::hardware_concurrency();
+	//std::cout << n << " concurrent threads are supported.\n";*/
 }
 
